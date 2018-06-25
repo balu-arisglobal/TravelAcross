2a6d721a05bfd036d571400837c5d84c044fe395
Balasubramanyam <balumatta01@gmail.com>
2018-06-25 08:03:15 +0530

    deleted flask project

--- run.py
+++
@@ -1,8 +0,0 @@
-from travel_across import app
-import os
-import json
-
-if __name__ == '__main__':
-    app.secret_key = os.urandom(12)
-    config = json.load(open("./config.json", "r"))
-    app.run(host=config["host"], port=config["port"], debug=config["debugMode"])
